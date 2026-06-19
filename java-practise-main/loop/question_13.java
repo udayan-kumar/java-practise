0de19fc15e172eb70cf35bc02b2a6bf0620c62dd
udayan-kumar <udayanmannusingh01@gmail.com>
2026-06-19 08:29:12 +0530

    new question

--- java-practise-main/loop/question_13.java
+++ java-practise-main/loop/question_13.java
@@ -3,5 +3,6 @@ package loop;
 public class question_13 {
     public static void main(String[] args) {
         System.out.println("udayan singh");
+        System.out.println("udayan kumar");
     }
 }
