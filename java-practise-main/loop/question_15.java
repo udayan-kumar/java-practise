efa3f6a29786f199d95e2a6364e5c958ff0f8912
udayan-kumar <udayanmannusingh01@gmail.com>
2026-06-19 13:04:42 +0530

    add soln

--- java-practise-main/loop/question_15.java
+++ java-practise-main/loop/question_15.java
@@ -1,7 +1,24 @@
+//find the greatest common divisior of two integer using a loop
+
 package loop;
+
+import java.util.Scanner;
+import javax.sound.sampled.SourceDataLine;
 
 public class question_15 {
     public static void main(String[] args) {
-        
+        Scanner ud = new Scanner(System.in);
+
+        int n = ud.nextInt();
+        int num = ud.nextInt();
+
+        for(int i =1; i<=n; i++){
+            int rem = n%num;
+            int hcf = num%rem;
+            
+            num = hcf;
+            System.out.println(num);
+        }
     }
 }
+
