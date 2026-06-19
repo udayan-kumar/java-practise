b253ef0a9ea6a57402cab722d60603aa97c22727
udayan-kumar <udayanmannusingh01@gmail.com>
2026-06-19 09:09:17 +0530

    new question

--- java-practise-main/loop/question_13.java
+++ java-practise-main/loop/question_13.java
@@ -1,8 +1,23 @@
 package loop;
 
+import java.util.Scanner;
+
 public class question_13 {
     public static void main(String[] args) {
-        System.out.println("udayan singh");
-        System.out.println("udayan kumar");
+        Scanner ud = new Scanner(System.in);
+
+        System.out.print("enter the number: ");
+        int n = ud.nextInt();
+
+        for(int i =1; i<=n; i++){
+            if(n%i == 0){
+              for(int j= 1; j<=i; j++){
+                int div= n/j;
+                System.out.println(div);
+              }
+                
+              }
+            
+        }
     }
 }
